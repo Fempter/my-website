8d79122786b9a40f721221d703cad72cba7615ef
Dawid Haniewicz <fempter@gmail.com>
2021-09-12 16:06:35 +0200

    Init "Hello world" template based on Spring Boot

---
+++ src/main/java/com/fempter/mywebsite/controller/MyWebsiteController.java
@@ -0,0 +1,29 @@
+package com.fempter.mywebsite.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyWebsiteController {
+
+    @Value("${hello.message}")
+    private String message;
+
+    @GetMapping("/")
+    public String helloMessage(Model model) {
+        model.addAttribute("message", message);
+        return "hello-world"; //view name
+    }
+
+    // pass some parameter like /hello?name=thymeleaf
+    @GetMapping("/hello")
+    public String helloWithParam(@RequestParam(name = "name", defaultValue = "springboot")
+            String name, Model model) {
+        model.addAttribute("message", name);
+        return "hello-world"; //view name
+    }
+
+}
---
+++ src/test/java/com/fempter/mywebsite/MyWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fempter.mywebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
