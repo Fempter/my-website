8f2b52d4aca89afd70e5f1c3b6c5e1c458b912c7
Dawid Haniewicz <fempter@gmail.com>
2023-05-30 09:16:09 +0200

    Improve email input, improve styles, add email lib, add some logic to handle sending messages (currently still disables)

---
+++ src/main/java/com/fempter/mywebsite/service/EmailService.java
@@ -0,0 +1,14 @@
+package com.fempter.mywebsite.service;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+}
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,30 +1,50 @@
-let submitButton = document.querySelector('#submit-button');
+let submitButton = document.querySelector('#submit-button-wrapper');
 
-function emailValidate(email) {
+function validateEmail(email) {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
 }
 
+function validateName(name) {
+    return name && String(name).trim() !== "";
+}
+
+function validateMessage(message) {
+    return message && String(message).trim() !== "";
+}
+
 function clickListener(event) {
-    event.preventDefault();
-    let emailInput = document.querySelector('#email');
-    let messageInput = document.querySelector('#message');
+    let emailInput = document.querySelector('#email')?.value;
+    let nameInput = document.querySelector('#name')?.value;
+    let messageInput = document.querySelector('#message')?.value;
 
-    let emailText = emailInput.value;
-    let messageText = messageInput.value;
-
-    if (emailValidate(emailText) !== true) {
-        window.alert("You have entered an invalid email address!")
+    if (!validateEmail(emailInput)) {
+        window.alert("You have entered an invalid email address")
+        event.preventDefault();
         return;
     }
-    let alert = "Hey " + emailText + ". Thanks for your message: \"" + messageText + "\"\n\n"
-        + "Email send function is not implemented yet, therefore it will be not sent."
+    if (!validateName(nameInput)) {
+        window.alert("Your name is blank")
+        event.preventDefault();
+        return;
+    }
+    if (!validateMessage(messageInput)) {
+        window.alert("Your message is blank")
+        event.preventDefault();
+        return;
+    }
 
-    window.alert(alert);
+    if (true) {
+        event.preventDefault();
+        let alert = "Email send function is not implemented yet, therefore it will be not sent."
+        window.alert(alert);
+        return
+    }
+
+    window.alert("Your message have been successfully sent")
 }
 
 window.onload = function () {
-    document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+    document.getElementById("currentYear").innerHTML = String(new Date().getFullYear());
 }
-
 
 submitButton.addEventListener('click', clickListener);
